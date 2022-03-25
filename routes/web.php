1bd2c35fc6e8ab92410b9ada82ccd528688fda1b
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-25 16:12:54 +0100

    added PostController

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,14 @@ Route::get('/', function () {
 
 Auth::routes(['register' => false]);
 
-Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
+
+Route::middleware('auth')
+->prefix('admin')
+->name('admin.')
+->namespace('Admin')
+->group(function(){
+    Route::get('/', 'HomeController@index')->name('home');
+    Route::resource('posts', 'PostController');
+
+});
+
