30d26444cdce97ada5194bbef087596fac273fa1
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-25 15:39:47 +0100

    change rout name

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('guest.home');
+})->name('guest.home');
 
-Auth::routes();
+Auth::routes(['register' => false]);
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/admin', 'Admin\HomeController@index')->name('admin.home');
