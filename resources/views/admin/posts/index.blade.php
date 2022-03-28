f456db1dc26db208c6f5d450e1269d59e2ebd593
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-28 15:36:30 +0200

    added destroy controller post + button form delete

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -10,30 +10,39 @@
         <main>
             <table class="table border">
                 <thead>
-                  <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Titolo</th>
-                    <th scope="col">Slug</th>
-                    <th scope="col">Creato il..</th>
-                    <th scope="col"></th>
-                  </tr>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Titolo</th>
+                        <th scope="col">Slug</th>
+                        <th scope="col">Creato il..</th>
+                        <th scope="col"></th>
+                    </tr>
                 </thead>
                 <tbody>
                     @forelse ($posts as $post)
-                    <tr>
-                      <th scope="row">{{ $post->id }}</th>
-                      <td>{{ $post->title }}</td>
-                      <td>{{ $post->slug }}</td>
-                      <td>{{ $post->created_at }}</td>
-                      <td><a class="btn btn-primary " href="{{ route('admin.posts.show', $post->id) }}">Vedi</a></td>
-                    </tr>                      
+                        <tr>
+                            <th scope="row">{{ $post->id }}</th>
+                            <td>{{ $post->title }}</td>
+                            <td>{{ $post->slug }}</td>
+                            <td>{{ $post->created_at }}</td>
+                            <td class="d-flex"><a class="btn btn-sm btn-primary mx-2"
+                                    href="{{ route('admin.posts.show', $post->id) }}">Vedi</a>
+                                <form action="{{ route('admin.posts.destroy', $post->id) }}" method="POST">
+                                  @method('DELETE')
+                                  @csrf
+                                    <button class="btn btn-sm btn-danger " type="submit">Cancella</button>
+                                </form>
+                            </td>
+                        </tr>
                     @empty
-                    <tr>
-                        <td> <h2>non ci sono post</h2></td>
-                    </tr>
+                        <tr>
+                            <td>
+                                <h2>non ci sono post</h2>
+                            </td>
+                        </tr>
                     @endforelse
                 </tbody>
-              </table>
+            </table>
         </main>
     </div>
 @endsection
