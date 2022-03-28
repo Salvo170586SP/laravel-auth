8f6f8840d233f349a8a2c6ccd5d3df16fd9453b5
Salvo170586SP <pitanza.salvatore.86@gmail.com>
2022-03-28 16:09:44 +0200

    added page create posts

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="header-posts mb-5 d-flex justify-content-between align-items-center">
+            <h1>Crea post</h1>
+        </div>
+        <form action="{{ route('admin.posts.store') }}" method="POST">
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" class="form-control" id="title" placeholder="Inserisci titolo" name="title">
+            </div>
+            <div class="mb-3">
+                <label for="image" class="form-label">Immagine</label>
+                <input type="url" class="form-control" id="image" placeholder="Inserisci titolo" name="image">
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Aggiungi descrizione</label>
+                <textarea class="form-control" id="content" rows="5" name="content"></textarea>
+            </div>
+            <button type="submit" class="btn btn-secondary">Aggiungi</button>
+        </form>
+    </div>
+@endsection
